04b26a04e8ef53dd11fd0c89c8fa8f9a3792f0ff
root <jonahcragun@gmail.com>
2024-09-30 18:15:21 -0600

    added frame for functions

---
+++ assembler/file_handle.py
@@ -0,0 +1,11 @@
+
+
+
+# read assembly file into string
+# file is refrence to string where the file will be stored after reading
+def read_file(file_name: str, file: str) -> int:
+    pass
+
+# write bytes to binary file
+def write_bin(memory: list) -> int:
+    pass
---
+++ assembler/parse.py
@@ -0,0 +1,13 @@
+
+
+# parse data section
+def parse_data(memory: list):
+    pass
+
+# parse instruction section
+def parse_instr(memory: list):
+    pass
+
+# replace labels with addresses
+def second_pass(memory: list):
+    pass
