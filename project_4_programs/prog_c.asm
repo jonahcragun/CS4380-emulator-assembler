8b0c15b8c8a4101f94986586acaa845aa7d0c75f
root <jonahcragun@gmail.com>
2024-12-03 21:36:22 -0700

    completed program 3

--- project_4_programs/prog_c.asm
+++ project_4_programs/prog_c.asm
@@ -46,16 +46,41 @@ fib pshr fp
     pshr r2
 
     ; get param
-    mov r2, fp
-    addi r2, r2, #12
-    ildr r2, r2
+    mov r0, fp
+    addi r0, r0, #12
+    ildr r0, r0
 
+    ; ********************
     ; calculate fib term
+    ; ********************
     
-    ; put return value on stack
-    mov r2, fp
-    addi r2, r2, #8
-    istr r0, r2
+    ; base case
+    subi r1, r0, #1
+    bgt r1, c1
+    jmp end
+
+    ; recursive call 1 (n has already been decremented)
+c1  pshr r1
+    subi sp, sp, #4
+    call fib
+    popr r0
+    popr r2
+
+    ; recursive call 2
+    subi r1, r1, #1
+    pshr r1
+    subi sp, sp, #4
+    call fib
+    popr r1
+    popr r2
+
+    ; add results
+    add r0, r0, r1
+    
+    ; save return value
+end mov r1, fp
+    addi r1, r1, #8
+    istr r0, r1
 
     ; put everything back
     popr r2
